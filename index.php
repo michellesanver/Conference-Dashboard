4ea65ee0705cd07f37c6a711b17ace87b29624a7
Michelle Sanver <geekie@Kalle.local>
2013-10-26 13:39:39 +0200

    There's now header and footer widgets that are outside the main area so you can't drag them around.

--- index.php
+++ index.php
@@ -6,44 +6,77 @@
   <title>Conference Dashboard</title>
 
   <link rel="stylesheet" href="css/smoothness/jquery-ui-1.10.3.custom.min.css" />
+  <link rel="stylesheet" href="css/dashboardstyle.css" />
   <script src="js/jquery/jquery-1.9.1.js"></script>
   <script src="js/jquery/jquery-ui-1.10.3.custom.min.js"></script>
+
   <script src="js/DashboardApp.js"></script>
 
 </head>
 
 <body>
- 
+ 	<?php include('Widget.php'); ?>
+
 	<h1>Conference Dashboard</h1>
-	 
-	<div id="widgetarea">
+	
+	<div class="widgetheader">
+		<?php
+			$headerwidgets = array();
+			$headerwidgets[] = new Widget("Emergency Ticker", "Important information...", 0, false);
+		?>
+
+		<?php foreach($headerwidgets as $widget): ?>
+			<div class="portlet">
+
+				<div class="portlet-header"><?php echo $widget->getHeader(); ?></div>
+				<div class='portlet-content'><?php echo $widget->getOutput(); ?></div>
+
+			</div>
+		<?php endforeach; ?>
+	</div>
+
+	<div class="widgetarea">
 
 		<!-- Temp data, this should be generated -->
 		<?php
-			include('Widget.php');
-
 			$widgets = array();
-			$widgets[] = new Widget("Emergency Ticker", "Important information...", 0, false);
 			$widgets[] = new Widget("Now / Next", "Schedule...");
 			$widgets[] = new Widget("Social Info", "Social information :D");
 			$widgets[] = new Widget("Map", "Beautiful map!");
 			$widgets[] = new Widget("Break time", "Break countdown");
 			$widgets[] = new Widget("Menu", "Menu with allergy information");
-			$widgets[] = new Widget("Sponsors", "Sponsorlogos", 20, false);
 
 		?>
 
 		<!-- Dynamic Widgetarea -->
 		<?php foreach($widgets as $widget): ?>
 
-			<div class="collapsible <?php if( $widget->isSortable() == false ): ?>not-sortable<?php endif; ?>">
-				<h3 class="collapsible"><?php echo $widget->getHeader(); ?></h3>
-				<div><?php echo $widget->getOutput(); ?></div>
+			<div class="portlet">
+
+				<div class="portlet-header"><?php echo $widget->getHeader(); ?></div>
+				<div class='portlet-content'><?php echo $widget->getOutput(); ?></div>
+
 			</div>
 
 		<?php endforeach; ?>
 
 	</div>
- 
+ 	
+ 	<div class="widgetfooter">
+		<?php
+			$footerwidgets = array();
+			$footerwidgets[] = new Widget("Sponsors", "Sponsorlogos", 20, false);
+		?>
+
+		<?php foreach($footerwidgets as $widget): ?>
+			<div class="portlet">
+
+				<div class="portlet-header"><?php echo $widget->getHeader(); ?></div>
+				<div class='portlet-content'><?php echo $widget->getOutput(); ?></div>
+
+			</div>
+		<?php endforeach; ?>		
+ 	</div>
+
 </body>
 </html>

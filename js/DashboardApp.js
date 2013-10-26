4ab14277a1d5abc0d178bf08dcbf28da1b88cfbc
Michelle Sanver <geekie@Kalle.local>
2013-10-26 13:39:55 +0200

    Collapse and sort.

--- js/DashboardApp.js
+++ js/DashboardApp.js
@@ -1,12 +1,19 @@
-  $(function() {
-  	$( "#widgetarea" )
-    .sortable({
-      	items: "div:not(.not-sortable)"
+$(function() {
+    $( ".widgetarea" ).sortable({
+      connectWith: ".widgetarea"
     });
- 	
- 	$('.collapsible').on('click', function() {
-   		$(this).children("div").slideToggle();
-	});
-
-
-  });
+ 
+    $( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".portlet-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
+        .end()
+      .find( ".portlet-content" );
+ 
+    $( ".portlet-header .ui-icon" ).click(function() {
+      $( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
+      $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
+    });
+ 
+    $( ".widgetarea" ).disableSelection();
+});
